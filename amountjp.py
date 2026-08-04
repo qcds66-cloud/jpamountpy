5df3f9d6f497ada9c345f29ec61ed59f26ef7ad2
qcds66-cloud <qcds66@gmail.com>
2026-08-04 23:18:02 +0800

    Update amountjp.py

--- amountjp.py
+++ amountjp.py
@@ -76,7 +76,7 @@ st.markdown(f"""
     
     /* 🔥 縮小 Streamlit 預設的區塊間距 */
     div[data-testid="stVerticalBlock"] {{
-        gap: 3rem !important; 
+        gap: 1.5rem !important; 
     }}
     /* 🔥 將下拉選單底部的空間進一步壓縮 */
     div[data-testid="stSelectbox"] {{
